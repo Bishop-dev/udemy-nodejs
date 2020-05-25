d2330ee8fcd7d2036a0f0758a7aae983617d12ab
Bishop-dev <alex.bishop@yandex.ru>
2020-05-25 23:59:27 +0300

    30 - sending responses

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => console.log(req));
+const server = http.createServer((req, res) => {
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'application/json');
+    res.write('{"test": "user"}');
+    res.end();
+});
 
 server.listen(3000);
