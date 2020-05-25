015fdfb1c86311db63e6802df6762cf78841907f
Bishop-dev <alex.bishop@yandex.ru>
2020-05-26 00:13:51 +0300

    33 - redirecting requests

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const server = http.createServer((req, res) => {
         res.setHeader('Content-Type', 'application/json');
         res.write('{"test": "user"}');
     }
+    if (req.url === '/message' && req.method === 'POST') {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+    }
     res.end();
 });
 
